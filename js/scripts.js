75ee7bd8a2fbb0deb4910b53fe395b84ee36a699
sabts <sabrinataraschi@gmail.com>
2025-03-19 11:26:33 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 const nameLenghtDetector = (name) =>{
   return name.lenght >= 7 ? 'Nombre largo' : 'Nombre corto';
 }
-console.log (nameLenghtDetector.lenght (Ana))
+console.log (nameLenghtDetector(Ana))
